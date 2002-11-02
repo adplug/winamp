012e26535770553f49ceebed4372134d796488fa
Simon Peter <dn.tlp@gmx.net>
2002-11-02 13:11:27 +0000

    Commented out IDC_HAND and changed into IDC_ARROW until riven finds something compatible. ;)

--- src/gui_link.cpp
+++ src/gui_link.cpp
@@ -174,7 +174,7 @@ void GuiCtrlHyperlink::set_normal(int i)
 void GuiCtrlHyperlink::set_hover(int i)
 {
 	work.state[i] = HL_HOVER;
-	SetCursor(LoadCursor(NULL,IDC_HAND));
+	SetCursor(LoadCursor(NULL,/*IDC_HAND*/ IDC_ARROW));
 	paint(i);
 }
 
