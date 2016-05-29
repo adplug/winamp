ba801875e9e6eb1e26154a6198dbcf1045aa116f
Adam Nielsen <malvineous@shikadi.net>
2016-05-29 21:16:30 +1000

    Update resources to VS2015 style, with updated dialogs from previous commit
    I'm putting this as a separate commit because it changes the resource file so
    significantly.  Unfortunately this just seems to be the way VS2015 does things
    now.

--- src/resource.h
+++ src/resource.h
@@ -1,3 +1,7 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by in_adlib.rc
+//
 /*
   Copyright (c) 1999 - 2006 Simon Peter <dn.tlp@gmx.net>
   Copyright (c) 2002 Nikita V. Kalaganov <riven@ok.ru>
@@ -36,23 +40,30 @@
 #define IDC_FREQ2                       1002
 #define IDC_FREQ3                       1003
 #define IDC_FREQ4                       1004
-#define IDC_FREQC                       1005
-#define IDC_FREQC_VALUE                 1006
-#define IDC_QUALITY8                    1007
-#define IDC_QUALITY16                   1008
-#define IDC_MONO                        1009
-#define IDC_STEREO                      1010
-#define IDC_OUTTS                       1011
-#define IDC_OUTKS                       1012
-#define IDC_OUTDISK                     1013
-#define IDC_TESTLOOP                    1017
-#define IDC_PRIORITY                    1019
-#define IDC_FORMATLIST                  1020
-#define IDC_FTSELALL                    1021
-#define IDC_FTDESELALL                  1022
-#define IDC_STDTIMER                    1023
-#define IDC_USEDB                       1024
-#define IDC_DIRECTORY                   1027
+#define IDC_FREQ5                       1005
+#define IDC_FREQC                       1006
+#define IDC_FREQC_VALUE                 1007
+#define IDC_QUALITY8                    1008
+#define IDC_QUALITY16                   1009
+#define IDC_MONO                        1010
+#define IDC_STEREO                      1011
+#define IDC_SURROUND                    1012
+#define IDC_OUTWO                       1013
+#define IDC_OUTTS                       1014
+#define IDC_OUTKS                       1015
+#define IDC_OUTDISK                     1016
+#define IDC_ALTSYNTH                    1017
+#define IDC_OUTWO2                      1018
+#define IDC_OUTTS2                      1019
+#define IDC_TESTLOOP                    1020
+#define IDC_FASTSEEK                    1021
+#define IDC_PRIORITY                    1022
+#define IDC_FORMATLIST                  1023
+#define IDC_FTSELALL                    1024
+#define IDC_FTDESELALL                  1025
+#define IDC_STDTIMER                    1026
+#define IDC_USEDB                       1027
+#define IDC_DIRECTORY                   1028
 #define IDC_TITLE                       1032
 #define IDC_AUTHOR                      1033
 #define IDC_SUBSONGSLIDER               1035
@@ -74,8 +85,6 @@
 #define IDC_LICENSE                     1054
 #define IDC_URL_GNU                     1060
 #define IDC_URL_WV                      1061
-#define IDC_HARMONIC                    1062
-#define IDC_DUELSYNTH                   1063
 #define IDC_CTABS                       1070
 #define IDC_PLUGIN_VER                  1071
 #define IDC_URL_SP                      1072
@@ -88,3 +97,19 @@
 #define IDC_DATABASE                    1079
 #define IDC_FTWORKAROUND                1080
 #define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#define APSTUDIO_READONLY_SYMBOLS
+#include <windows.h>
+#include "version.h"
+#undef APSTUDIO_READONLY_SYMBOLS
